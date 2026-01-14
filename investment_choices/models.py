89ec0b870aba9f673eeb6b62d60041ae0eb84860
GeorgeWork1005 <GeorgeWork1005@gmail.com>
2026-01-14 11:05:28 +0800

    update

--- investment_choices/models.py
+++ investment_choices/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+from investment_types.models import InvestmentType
+
 # Create your models here.
 
 class InvestmentChoice (models.Model) :
     fund_name = models.CharField (max_length=50)
-    # fund_type = 
+    fund_type = models.ForeignKey (InvestmentType, on_delete=models.DO_NOTHING)
+    fund_description = models.CharField (max_length=500, blank=True, null=True)
+
+    def __str__ (self) :
+        return self.fund_name
+
+
